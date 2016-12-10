70c1ebb0e3ce640c0eb31da7e951a8c1b5bdd3da
Xindi Shang <shangxin@comp.nus.edu.sg>
2016-12-10 11:08:28 +0800

    Add compute_separate_score for moving and static resutls.

---
+++ compute_seperate_scores.py
@@ -0,0 +1,37 @@
+import os, sys
+import pickle
+from utils import *
+from evaluate import *
+
+if __name__ == '__main__':
+  working_root = '../'
+  nreturn = 2000
+
+  vinds = get_vinds(os.path.join(working_root, 'datalist.txt'), int(sys.argv[1]), int(sys.argv[2]))
+
+  for i, vind in enumerate(vinds):
+    print 'Processing %dth video...' % i
+
+    with open(os.path.join(working_root, 'our_results', '%s.pkl' % vind), 'r') as fin:
+      data = pickle.load(fin)
+      mtracks = data['mtracks']
+      stracks = data['stracks']
+      scale = data['scale']
+
+    gt_tracks = get_gt_tracks(os.path.join(working_root, 'annotations/%s.xml' % vind), scale)
+
+    mresults = evaluate_track(mtracks, gt_tracks)
+    with open(os.path.join(working_root, 'our_moving_results', '%s.txt' % vind), 'w') as fout:
+      ss = 0.
+      for gt_id, result in mresults.iteritems():
+        ss += result[1]
+        print >> fout, 'gt %d matches track %s with score %f' % (gt_id, result[0], result[1])
+      print >> fout, 'average score %f' % (ss / len(mresults),)
+
+    sresults = evaluate_track(stracks[:nreturn], gt_tracks)
+    with open(os.path.join(working_root, 'our_static_results', '%s.txt' % vind), 'w') as fout:
+      ss = 0.
+      for gt_id, result in sresults.iteritems():
+        ss += result[1]
+        print >> fout, 'gt %d matches track %s with score %f' % (gt_id, result[0], result[1])
+      print >> fout, 'average score %f' % (ss / len(sresults),)
