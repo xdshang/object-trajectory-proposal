2a6d0b88d828726ded2606af0a491e23fcc0ffc4
Xindi Shang <shangxin@comp.nus.edu.sg>
2016-12-13 01:01:56 +0800

    Update baseline_proposals to fit mcg method

--- baseline_proposals.py
+++ baseline_proposals.py
@@ -21,18 +21,25 @@ def generate_proposals(vind, nreturn, baseline, vsize = 240, working_root = '.')
 
   bfilter = BBoxFilter(size[0] * size[1] * 0.001, size[0] * size[1], 0.1)
   pinit = len(frames) / 2
-  bbs = sio.loadmat(glob.glob('%s/%s_proposals/*/%s*' % \
-      (working_root, baseline, vind))[0])['bbs']
-  if bbs.shape[0] > 1:
+
+  if 'edgebox' in baseline:
+    bbs = sio.loadmat(glob.glob('%s/%s_proposals/*/%s*' % \
+        (working_root, baseline, vind))[0])['bbs']
     bbs = bbs[pinit][0]
-  else:
-    bbs = None
-  bbs[:, :2] -= 1
-  bbs[:, :4] *= scale
+    bbs[:, :2] -= 1
+    bbs[:, :4] *= scale
+  elif 'mcg' in baseline:
+    bbs = sio.loadmat(glob.glob('%s/%s_proposals/%s*' % \
+        (working_root, baseline, vind))[0])['new_bbs']
+    bbs[:, :2] -= 1
+    bbs[:, :4] *= scale * orig_size[1] / 360
 
   tracks = []
   for i in range(min(bbs.shape[0], nreturn)):
     bbox_init = truncate_bbox(bbs[i, :4], size[1], size[0])
+    bbox_init = round_bbox(bbox_init)
+    if not bfilter(bbox_init):
+      continue
     track = MovingTrack(pinit, bbox_init, frames[pinit])
     for ind in range(pinit + 1, len(frames)):
       bbox = track.predict(frames[ind])
@@ -46,8 +53,8 @@ def generate_proposals(vind, nreturn, baseline, vsize = 240, working_root = '.')
         break
     track.terminate()
     tracks.append((bbs[i, 4] * track.length(), track))
-    # if i % 20 == 0:
-    #   print 'Tracking %dth frame, total tracks %d' % (i, len(tracks))
+    if i % 20 == 0:
+      print 'Tracking %dth frame, total tracks %d' % (i, len(tracks))
 
   tracks = [track for s, track in sorted(tracks, key = lambda x: x[0], reverse = True)]
   return tracks, scale
@@ -55,23 +62,24 @@ def generate_proposals(vind, nreturn, baseline, vsize = 240, working_root = '.')
 
 if __name__ == '__main__':
   working_root = '../'
-  baseline = sys.argv[1]
+  saving_root = sys.argv[1]
+  baseline = sys.argv[2]
   nreturn = 2000
 
-  assert os.path.exists(os.path.join(working_root, '%s_results' % baseline)), \
+  assert os.path.exists(os.path.join(saving_root, '%s_results' % baseline)), \
       'Directory for results of %s not found' % baseline
-  vinds = get_vinds(os.path.join(working_root, 'datalist.txt'), int(sys.argv[2]), int(sys.argv[3]))
+  vinds = get_vinds(os.path.join(working_root, 'datalist.txt'), int(sys.argv[3]), int(sys.argv[4]))
 
   for i, vind in enumerate(vinds):
     print 'Processing %dth video...' % i
 
     tracks, scale = generate_proposals(vind, nreturn, baseline, working_root = working_root)
-    with open(os.path.join(working_root, '%s_results' % baseline, '%s.pkl' % vind), 'wb') as fout:
+    with open(os.path.join(saving_root, '%s_results' % baseline, '%s.pkl' % vind), 'wb') as fout:
       pickle.dump({'tracks': tracks, 'scale': scale}, fout)
 
     gt_tracks = get_gt_tracks(os.path.join(working_root, 'annotations/%s.xml' % vind), scale)
     results = evaluate_track(tracks, gt_tracks)
-    with open(os.path.join(working_root, '%s_results' % baseline, '%s.txt' % vind), 'w') as fout:
+    with open(os.path.join(saving_root, '%s_results' % baseline, '%s.txt' % vind), 'w') as fout:
       ss = 0.
       for gt_id, result in results.iteritems():
         ss += result[1]
